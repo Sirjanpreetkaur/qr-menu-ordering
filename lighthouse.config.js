3322452ae71b782bb8367eeb2afc356e26af58b0
Sirjanpreetkaur <sirjanpreetkaur43@gmail.com>
2025-08-08 02:32:55 +0530

    lighthouse changes

---
+++ lighthouse.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: ['http://localhost:3000'],
+      numberOfRuns: 1,
+      startServerCommand: 'npm start',
+      startServerReadyPattern: 'Compiled successfully',
+      startServerReadyTimeout: 60000,
+      output: 'none'  
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['warn', { minScore: 0.9 }],
+        'first-contentful-paint': ['warn', { maxNumericValue: 2000 }],
+        'largest-contentful-paint': ['warn', { maxNumericValue: 2500 }],
+        'cumulative-layout-shift': ['warn', { maxNumericValue: 0.1 }]
+      }
+    }
+  }
+};
